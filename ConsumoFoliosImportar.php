f4d2a3c0dee2f56801a1c4126024b09d25510227
FacTrónica <webmaster@factronica.cl>
2019-09-09 17:13:07 -0300

    Create ConsumoFoliosImportar.php

---
+++ ConsumoFoliosImportar.php
@@ -0,0 +1,57 @@
+<?php 
+# 
+# DATOS DE CARATULA
+$Caratula["RutEmisor"]="77777777-7";
+$Caratula["FchInicio"]="2017-03-24";
+#
+# CONSUMO MULTI-CAJA Y MULTI-SUCURSAL ( 
+$FACTRONICA["ID_SUCURSAL"]="1"; // ID DE LA SUCURSAL
+$FACTRONICA["ID_CAJA"]="3"; // ID DE LA CAJA
+$FACTRONICA["ID_CAJERO"]="143"; // ID DEL CAJERO
+#
+# DATOS DE RESUMEN BOLETA ELECTRONICA
+$Resumen["TipoDocumento"]["39"]="39";
+$Resumen["MntNeto"]["39"]="100000";
+$Resumen["MntIva"]["39"]="19000";
+$Resumen["TasaIVA"]["39"]="19";
+$Resumen["MntExento"]["39"]="0";
+$Resumen["MntTotal"]["39"]="119000";
+$Resumen["FoliosEmitidos"]["39"]="20";
+$Resumen["FoliosAnulados"]["39"]="0";
+$Resumen["FoliosUtilizados"]["39"]="20";
+$Resumen["RangoUtilizadosInicial"]["39"]="1001";
+$Resumen["RangoUtilizadosFinal"]["39"]="1020";
+$Resumen["RangoAnuladosInicial"]["39"]="0";
+$Resumen["RangoAnuladosFinal"]["39"]="0";
+
+# DATOS DE RESUMEN BOLETA EXENTA ELECTRONICA
+$Resumen["TipoDocumento"]["41"]="41";
+$Resumen["MntNeto"]["41"]="";
+$Resumen["MntIva"]["41"]="";
+$Resumen["TasaIVA"]["41"]="";
+$Resumen["MntExento"]["41"]="";
+$Resumen["MntTotal"]["41"]="";
+$Resumen["FoliosEmitidos"]["41"]="0";
+$Resumen["FoliosAnulados"]["41"]="0";
+$Resumen["FoliosUtilizados"]["41"]="0";
+$Resumen["RangoUtilizadosInicial"]["41"]="";
+$Resumen["RangoUtilizadosFinal"]["41"]="";
+$Resumen["RangoAnuladosInicial"]["41"]="";
+$Resumen["RangoAnuladosFinal"]["41"]="";
+
+# DATOS DE RESUMEN NOTA DE CREDITO ELECTRONICA
+$Resumen["TipoDocumento"]["61"]="61";
+$Resumen["MntNeto"]["61"]="";
+$Resumen["MntIva"]["61"]="";
+$Resumen["TasaIVA"]["61"]="";
+$Resumen["MntExento"]["61"]="";
+$Resumen["MntTotal"]["61"]="";
+$Resumen["FoliosEmitidos"]["61"]="0";
+$Resumen["FoliosAnulados"]["61"]="0";
+$Resumen["FoliosUtilizados"]["61"]="0";
+$Resumen["RangoUtilizadosInicial"]["61"]="";
+$Resumen["RangoUtilizadosFinal"]["61"]="";
+$Resumen["RangoAnuladosInicial"]["61"]="";
+$Resumen["RangoAnuladosFinal"]["61"]="";
+ 
+?>
